64aa0c0e1553164ce73e08c68a50078d8b429cc1
hiroaqii <hiroaqii@gmail.com>
2022-08-13 20:17:26 +0900

    2.4 構文解析器の第一歩:let文

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"os/user"
 
-	"github.com/hiroaqii/monky/repl"
+	"github.com/hiroaqii/monkey/repl"
 )
 
 func main() {
--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/hiroaqii/monkey/ast"
 	"github.com/hiroaqii/monkey/lexer"
 	"github.com/hiroaqii/monkey/token"
@@ -11,14 +13,28 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -78,6 +94,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
